51e2a57612d9318d1b14dd8278f857e2484d8f8b
elena992 <elena.fernandez1992@gmail.com>
2023-01-16 18:46:14 +0100

    Update Recipe.model.js

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+  ingredientes: {
+    type: [String],
+  },
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;
